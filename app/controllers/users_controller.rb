6162b130855e2ecdb00960d999e4a93f10a1ceac
Kiran <kiran@ubuntu.(none)>
2013-04-12 15:19:03 +0200

    Finished the Signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  
+
+
+  def show
+  	#raise params.inspect
+  	@user = User.find(params[:id])
   end
+
+  def new
+     @user = User.new
+  end
+
+  def create
+   	 @user = User.new(params[:user])
+   	 if @user.save
+   	 	#redirect_to user_path(@user) it can also be done as follows
+   	 	flash[:success] = "Welcome to CreatorsMap"
+   	 	redirect_to @user
+   	 else
+   	 	render 'new'
+   	 end
+  end 
+
 end
+
